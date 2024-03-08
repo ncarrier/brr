438c5627d30ca621a3f50bef65dd3e1c9dffb590
Nicolas Carrier <Nicolas Carrier>
2024-03-08 22:15:42 +0100

    support for true
    adds some very basic command-line parsing, through the ability
    to parse argv[0]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::path::Path;
+use std::env;
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    return ExitCode::FAILURE;
+    let args: Vec<String> = env::args().collect();
+    let p = Path::new(&args[0]).file_name();
+    let s = p.expect("base name can't be None").to_str().expect("really?");
+    match s {
+        "false" => return ExitCode::FAILURE,
+        "true" => return ExitCode::SUCCESS,
+        _ => return ExitCode::FAILURE
+    }
 }
